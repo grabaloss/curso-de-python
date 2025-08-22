713c5f2c4c774624962c03bb95a0122a8c25271d
grabaloss <dgrabalos.lucas@gmail.com>
2025-08-22 15:13:38 -0300

    ex3

---
+++ curso_de_python/ex3.py
@@ -0,0 +1,11 @@
+#nome = input("QUal o seu nome? ")
+#altura = float(input("Qual a sua altura? "))
+#peso = float(input("Qual o seu peso? "))
+#imc = peso / (altura ** 2)
+#print(f"{nome} tem {altura:.2f} de altura e pesa {peso:.2f} kg. Seu IMC é {imc:.2f}.")
+
+#num1 = int(input("Digite um número: "))
+#num2 = int(input("Digite outro número: "))
+#soma = num1 + num2
+#par_ou_impar = "par" if soma % 2 == 0 else "ímpar"
+#print(f"A soma dos números é {soma} que é um número {par_ou_impar}.")
