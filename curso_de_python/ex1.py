9ed5a59577a6d4adcad22551d4d674c3b45b52b6
grabaloss <128254322+grabaloss@users.noreply.github.com>
2025-08-22 13:21:05 -0300

    Add files via upload

---
+++ curso_de_python/ex1.py
@@ -0,0 +1,6 @@
+nome = "Yuri Alberto Monteiro da Silva"
+altura = 1.82
+peso = 78
+imc = peso / altura ** 2
+resposta = f"{nome} tem {altura:.2f} de altura, pesa {peso} quilos e seu IMC é {imc}"
+print (resposta) 
