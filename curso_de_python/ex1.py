45f9be634d4e465c415b5a71a9c68c7f4b477e28
grabaloss <dgrabalos.lucas@gmail.com>
2025-08-22 13:32:24 -0300

    criado a descrição

--- curso_de_python/ex1.py
+++ curso_de_python/ex1.py
@@ -1,6 +1,7 @@
 nome = "Yuri Alberto Monteiro da Silva"
+descricao = "atacante do Corinthians"
 altura = 1.82
 peso = 78
 imc = peso / altura ** 2
-resposta = f"{nome} tem {altura:.2f} de altura, pesa {peso} quilos e seu IMC é {imc}"
+resposta = f"{nome}, {descricao} tem {altura:.2f} de altura, pesa {peso} quilos e seu IMC é {imc}"
 print (resposta) 
